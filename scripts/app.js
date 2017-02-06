1947dfad0bb91069b2646dcd9e30795790fd8d54
An Huynh <An Huynh>
2017-02-06 16:08:15 -0600

    adding some styling

--- scripts/app.js
+++ scripts/app.js
@@ -2,37 +2,44 @@ var beerArray = [];
 var searchUrl = "https://api.punkapi.com/v2/beers/";
 var newSearchUrl ="";
 
+// --------------searching by beer name------------
+
 $('#searchBtnName').on('click', function(){
+    console.log('hello');
+    $('.searchedBeer').empty();
     var userInput = $('#inputName').val().toLowerCase().trim();
-    var beerId = beerArray.indexOf(userInput) + 1;
-    $('.searchedBeer').append(
-        '<div>Name: ' + beerArray[beerId].name + '</div>' + 
-        '<div>Alcohol By Volume: ' + beerArray[beerId].abv + '%</div>' +
-        '<div>Summary: ' + beerArray[beerId].description + '</div>'
-        )
+    console.log(userInput);
+    var beerId = (beerArray.indexOf(userInput) + 1);
+    console.log(beerId);
+
+    for(var i = 0; i < beerArray.length; i++) {
+        if(beerArray[i].name === userInput) {
+            console.log('found! ' + i);
+            
+        $('.searchedBeer').append(
+            '<div>Name: ' + beerArray[i].name + '</div>' + 
+            '<div>Alcohol By Volume: ' + beerArray[i].abv + '%</div>' +
+            '<div>Summary: ' + beerArray[i].description + '</div>'
+            )
+        }
+    }
+
+
 });
 
+// ---------------------searching bu ABV---------------------
+
 $('#searchBtnAbv').on('click', function(){
-    var userInput = Number($('#inputAbv').val().trim());
-    console.log(userInput);   
+    $('.searchedBeer').empty();
+    var userInput = Number($('#inputAbv').val().trim());   
     var beerIdArray = [];
-
-    console.log("length " + beerArray.length);
-    console.log(typeof beerArray[0].abv);
-    console.log(typeof userInput);
-
     for (var i = 0; i < beerArray.length; i++) {
         var abvs = beerArray[i].abv;
         if (abvs == userInput){
            beerIdArray.push(i); 
-          
         }
     }
-
-    console.log("array: " + beerIdArray);
-
     for(var j = 0; j < beerIdArray.length; j++) {
-        console.log("hello?");
         var n = beerIdArray[j];
         $('.searchedBeer').append(
             '<div>Name: ' + beerArray[n].name + '</div>' + 
@@ -41,6 +48,8 @@ $('#searchBtnAbv').on('click', function(){
             )
     }
 });
+
+// ----------------making ajax call to the API via Angular-----------
 
 angular
     .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource'])
@@ -53,31 +62,27 @@ angular
             method: 'GET',
             url: searchUrl
         }).then(function theDeets(response){
-            // var data = response;
-            var labelName = response.data[0].name;
-            console.log(response);
+                // var data = response;
+                var labelName = response.data[0].name;
+                console.log(response);
 
-            for(var i = 0; i < response.data.length; i++) {
-                var beerName = response.data[i].name.toLowerCase().trim();
-                var beerAbv = response.data[i].abv;
-                var beerDescription = response.data[i].description;
-                beerArray.push(
-                    {
-                        name: beerName,
-                        abv: beerAbv,
-                        description: beerDescription
-                    });
-                $('#beerLabels').append("<div class='singleBeer'>" + response.data[i].name + "</div>"); 
-            }
-            
-            console.log(beerArray);
-            // if any errors, pop this out
-            }, function theErrs(response){
-                return response;
-            });   
-    })
-    // .config(function($mdThemingProvider) {
-    //     $mdThemingProvider.theme('docs-dark', 'default')
-    //         .primaryPalette('yellow')
-    //         .dark();
-    // });
+                for(var i = 0; i < response.data.length; i++) {
+                    var beerName = response.data[i].name.toLowerCase().trim();
+                    var beerAbv = response.data[i].abv;
+                    var beerDescription = response.data[i].description;
+                    beerArray.push(
+                        {
+                            name: beerName,
+                            abv: beerAbv,
+                            description: beerDescription
+                        });
+                    $('#beerLabels').append("<div class='singleBeer'>" + response.data[i].name + "</div>"); 
+                    console.log(beerArray[i].abv)
+                }
+            });  
+    }).config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('docs-dark', 'default')
+            .primaryPalette('yellow')
+            .dark();
+    });
+

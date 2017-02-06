bf78f3215bc8b922448d35f85b7e1c5e26647fa7
An Huynh <An Huynh>
2017-02-06 11:55:37 -0600

    first push, making prelim ajax call via angular

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,30 @@
+
+
+
+
+
 angular
     .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource'])
     .controller('DemoCtrl', function($scope, $http, $resource) {
 
-
         // use $http or $resource
         // https://api.punkapi.com/v2/beers
 
-        
+        $http({
+            method: 'GET',
+            url: "https://api.punkapi.com/v2/beers"
+        }).then(function theDeets(response){
+            var data = response;
+            var labelName = response.data[0].name
+            // console.log(response.data.length);
+
+            for(var i = 0; i < response.data.length; i++) {
+                $('#beerLabels').append("<div>" + response.data[i].name + "</div>"); 
+                }
+            // if any errors, pop this out
+            }, function theErrs(response){
+                return response;
+            });   
     })
     .config(function($mdThemingProvider) {
         $mdThemingProvider.theme('docs-dark', 'default')

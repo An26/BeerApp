0253365b8653623e4a123aeb3d220df8eb9dc179
An Huynh <An Huynh>
2017-02-06 15:08:04 -0600

    functional app

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,19 @@
-
-
 var beerArray = [];
 var searchUrl = "https://api.punkapi.com/v2/beers/";
 var newSearchUrl ="";
-
 
 $('#searchBtnName').on('click', function(){
     var userInput = $('#inputName').val().toLowerCase().trim();
     var beerId = beerArray.indexOf(userInput) + 1;
     $('.searchedBeer').append(
-        '<div>' + beerArray[beerId].name + '</div>' + 
-        '<div>' + beerArray[beerId].abv + '</div>' +
-        '<div>' + beerArray[beerId].description + '</div>'
+        '<div>Name: ' + beerArray[beerId].name + '</div>' + 
+        '<div>Alcohol By Volume: ' + beerArray[beerId].abv + '%</div>' +
+        '<div>Summary: ' + beerArray[beerId].description + '</div>'
         )
 });
 
 $('#searchBtnAbv').on('click', function(){
-    var userInput = parseInt($('#inputAbv').val().trim());
+    var userInput = Number($('#inputAbv').val().trim());
     console.log(userInput);   
     var beerIdArray = [];
 
@@ -24,16 +21,24 @@ $('#searchBtnAbv').on('click', function(){
     console.log(typeof beerArray[0].abv);
     console.log(typeof userInput);
 
-    
-
     for (var i = 0; i < beerArray.length; i++) {
-        // console.log(beerArray[i].abv);
         var abvs = beerArray[i].abv;
-        if (abvs.indexOf(userInput) == true){
+        if (abvs == userInput){
            beerIdArray.push(i); 
-           console.log("array: " + beerIdArray);
-            // console.log(beerIdArray);
+          
         }
+    }
+
+    console.log("array: " + beerIdArray);
+
+    for(var j = 0; j < beerIdArray.length; j++) {
+        console.log("hello?");
+        var n = beerIdArray[j];
+        $('.searchedBeer').append(
+            '<div>Name: ' + beerArray[n].name + '</div>' + 
+            '<div>Alcohol By Volume: ' + beerArray[n].abv + '%</div>' +
+            '<div>Summary: ' + beerArray[n].description + '</div>'
+            )
     }
 });
 
@@ -62,7 +67,7 @@ angular
                         abv: beerAbv,
                         description: beerDescription
                     });
-                $('#beerLabels').append("<div>" + response.data[i].name + "</div>"); 
+                $('#beerLabels').append("<div class='singleBeer'>" + response.data[i].name + "</div>"); 
             }
             
             console.log(beerArray);
